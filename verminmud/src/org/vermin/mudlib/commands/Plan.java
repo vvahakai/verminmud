2dd84abc93396e2663088fda31aa2430f2632d98
lang.vili <lang.vili@6a8376a8-1244-0410-8987-ddb48aa94c23>
2009-02-03 17:39:10 +0000

    Modified parameters for plan command (show/set/clear/usage). Minor cosmetic changes to text formatting.

--- verminmud/src/org/vermin/mudlib/commands/Plan.java
+++ verminmud/src/org/vermin/mudlib/commands/Plan.java
@@ -16,33 +16,45 @@ public class Plan extends RegexCommand {
 
     public String[] getDispatchConfiguration() {  
         return new String[] {
-                "plan => showPlan(actor)",
+                "plan show => showPlan(actor)",
                 "plan clear => clearPlan(actor)",
-                "plan edit => editPlan(actor)"
+                "plan set => setPlan(actor)",
+                "plan => showUsage(actor)",
+                "plan (.*) => showUsage(actor)"
         };
     }
     
     public void showPlan(Player actor) {
-        actor.notice("Current plan:");
-        actor.notice(actor.getPlan());
+        if (actor.getPlan() == null) {   	
+        	actor.notice("You have currently no plan.");
+        } else {
+        	actor.notice("\nCurrent plan:");
+        	actor.notice(actor.getPlan());
+        	actor.notice("\n");
+        }
     }
     
     public void clearPlan(Player actor) {
         actor.setPlan(null);
+        actor.notice("Plan cleared.");
     }
     
-    public void editPlan(final Player actor) {
+    public void setPlan(final Player actor) {
         LineEditor.Listener listener =
             new LineEditor.Listener() {
                 public void done(String txt) {
                     actor.setPlan(txt);
+                    actor.notice("Plan set.");
                 }
 
                 public void canceled() {
                 }};    
         
-        LineEditor le = new LineEditor(actor, listener, 5); 
-            
-
+        LineEditor le = new LineEditor(actor, listener, 5, new String(">")); 
+    }
+    
+    public void showUsage(Player actor) {
+    	actor.notice("Usage: plan <show|set|clear>");
+    	actor.notice("For more information: help plan");
     }
 }
